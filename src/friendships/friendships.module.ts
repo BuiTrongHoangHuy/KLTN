fa7c44159cd6af6115a0229b26aa31cfeb452b4b
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2025-11-23 15:47:08 +0700

    fix deploy

--- src/friendships/friendships.module.ts
+++ src/friendships/friendships.module.ts
@@ -3,9 +3,10 @@ import { FriendshipsService } from './friendships.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Friendship } from './entities/friendship.entity';
 import { FriendshipsController } from './friendships.controller';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Friendship])],
+  imports: [TypeOrmModule.forFeature([Friendship]), NotificationsModule],
   providers: [FriendshipsService],
   exports: [FriendshipsService],
   controllers: [FriendshipsController],
