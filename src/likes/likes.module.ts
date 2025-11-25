1b085ad40c80b24e9adec8695a2d25918047f47e
BuiTrongHoangHuy <22520533@gm.uit.edu.vn>
2025-11-26 02:07:50 +0700

    add createNotification in comments, likes, friendships

--- src/likes/likes.module.ts
+++ src/likes/likes.module.ts
@@ -3,9 +3,14 @@ import { LikesService } from './likes.service';
 import { Like } from './entities/like.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentLike } from './entities/comment-like.entity';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { Post } from '../posts/entities/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Like, CommentLike])],
+  imports: [
+    TypeOrmModule.forFeature([Like, CommentLike, Post]),
+    NotificationsModule,
+  ],
   providers: [LikesService],
   exports: [LikesService],
 })
